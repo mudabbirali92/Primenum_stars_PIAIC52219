9a7fb6f3c4fb07fdd81790879481a8e4e3c50254
Mudabbir Ali <mudabbirali92@yahoo.com>
2020-05-15 10:58:55 +0500

    Contains prime number.js, stars.js and index html file

---
+++ prime_number.js
@@ -0,0 +1,19 @@
+var num=parseInt(prompt("Plz enter a number if it is prime"));
+
+if(num<1)
+{
+    alert("Sorry number is not prime");
+}
+else if(num%2===0 && num>0 && num!=2)
+{
+    alert("Sorry number is not prime");
+
+}
+else if(num==2)
+{
+    alert("Yes the number is prime"); 
+}
+else
+{
+    alert("Yes the number is prime");
+}
---
+++ stars.js
@@ -0,0 +1,9 @@
+var star="*";
+for (var row=1;row<7;row++)
+{
+    for (var col=0;col<row;col++)
+    {
+        document.write("*");
+    }
+    document.write("<br/>");
+}
